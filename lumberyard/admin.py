148a6a1d6552858e614ad5e3705f82917ac1711f
Oleksandr <vhsb721@email.com>
2026-07-16 13:20:38 +0200

    Add Supplier and Warehouse models

--- lumberyard/admin.py
+++ lumberyard/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Supplier, Warehouse
 
 admin.site.register(Category)
+admin.site.register(Supplier)
+admin.site.register(Warehouse)
--- lumberyard/models.py
+++ lumberyard/models.py
@@ -5,9 +5,32 @@ class Category(models.Model):
 
     class Meta:
         ordering = ["name"]
-        verbose_name = "Category"
-        verbose_name_plural = "Categories"
+        verbose_name = "category"
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
 
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "supplier"
+        verbose_name_plural = "suppliers"
+
+    def __str__(self):
+        return self.name
+
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "warehouse"
+        verbose_name_plural = "warehouses"
+
+    def __str__(self):
+        return self.name
